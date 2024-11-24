0d3acdb201bab25a8ac01322ddb82d8c859d4797
jayp1206 <jpatel1206@gmail.com>
2024-11-24 16:49:25 -0500

    added advanced audit policies

--- scriptv2.ps1
+++ scriptv2.ps1
@@ -40,7 +40,81 @@ function Enable-Firewall {
     Set-NetFirewallProfile -Name Private -DefaultOutboundAction Allow
     Set-NetFirewallProfile -Name Domain -DefaultOutboundAction Allow
 
-    Write-Host "Windows Firewall profiles configured successfully!"
+    Write-Host "Windows Firewall profiles configured successfully!" -ForegroundColor Green
+}
+
+
+function Enable-Audits {
+    # List of all advanced audit policies
+    $subcategories = @(
+    "Security State Change",
+    "Security System Extension",
+    "System Integrity",
+    "IPsec Driver",
+    "Other System Events",
+    "Logon",
+    "Logoff",
+    "Account Lockout",
+    "IPsec Main Mode",
+    "IPsec Quick Mode",
+    "IPsec Extended Mode",
+    "Special Logon",
+    "Other Logon/Logoff Events",
+    "Network Policy Server",
+    "User / Device Claims",
+    "Group Membership",
+    "File System",
+    "Registry",
+    "Kernel Object",
+    "SAM",
+    "Certification Services",
+    "Application Generated",
+    "Handle Manipulation",
+    "File Share",
+    "Filtering Platform Packet Drop",
+    "Filtering Platform Connection",
+    "Other Object Access Events",
+    "Detailed File Share",
+    "Removable Storage",
+    "Central Policy Staging",
+    "Sensitive Privilege Use",
+    "Non Sensitive Privilege Use",
+    "Other Privilege Use Events",
+    "Process Creation",
+    "Process Termination",
+    "DPAPI Activity",
+    "RPC Events",
+    "Plug and Play Events",
+    "Token Right Adjusted Events",
+    "Audit Policy Change",
+    "Authentication Policy Change",
+    "Authorization Policy Change",
+    "MPSSVC Rule-Level Policy Change",
+    "Filtering Platform Policy Change",
+    "Other Policy Change Events",
+    "User Account Management",
+    "Computer Account Management",
+    "Security Group Management",
+    "Distribution Group Management",
+    "Application Group Management",
+    "Other Account Management Events",
+    "Directory Service Access",
+    "Directory Service Changes",
+    "Directory Service Replication",
+    "Detailed Directory Service Replication",
+    "Credential Validation",
+    "Kerberos Service Ticket Operations",
+    "Other Account Logon Events",
+    "Kerberos Authentication Service"
+    )
+
+    # Set each to Sucess & Failure
+    foreach ($subcategory in $subcategories) {
+        auditpol /set /subcategory:"$subcategory" /success:enable /failure:enable
+
+    }
+
+    Write-Host "Advanced Audit Policies Configured Successfully!" -ForegroundColor Green
 }
 
 
@@ -48,10 +122,7 @@ function Enable-Firewall {
 
 
 
-
-
-
-$securityPolicy = Read-Host "Configure Password Policies? (y/n) "
+$securityPolicy = Read-Host "Configure Security Policies? (y/n) "
 if ($securityPolicy -eq 'y') { 
     Set-SecurityPolicies
 } else {
@@ -63,4 +134,11 @@ if ($firewall -eq 'y') {
     Enable-Firewall
 } else {
     Write-Host "Skipping Firewall Configuration" -ForegroundColor Yellow
+}
+
+$auditing = Read-Host "Configure Advanced Audit Policies? (y/n) "
+if ($auditing -eq 'y') { 
+    Enable-Audits
+} else {
+    Write-Host "Skipping Advanced Audit Policies" -ForegroundColor Yellow
 }   
