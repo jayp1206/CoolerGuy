0f00058bd6da357037bf7e6b559d9adba0a3d412
jayp1206 <jpatel1206@gmail.com>
2024-12-04 10:41:37 -0500

    modified local security policy to only use template instead of merging cfg files

--- scriptv3.ps1
+++ scriptv3.ps1
@@ -2,24 +2,8 @@ function Set-SecurityPolicies {
     # Copy current secpol.cfg file
     secedit /export /cfg original_secpol.cfg
 
-    # Load template and current configs into memory
-    $templateContent = Get-Content template_secpol.cfg
-    $originalContent = Get-Content original_secpol.cfg
-
-    # Take User Privilege Assignments from original config
-    $privilegeRightsStart = ($originalContent | Select-String -Pattern "^\[Privilege Rights\]$").LineNumber - 1
-    $privilegeRightsContent = $originalContent[$privilegeRightsStart..($originalContent.Count - 1)]
-
-    # Combine template config and existing user rights into a merged config
-    $mergedContent = @()
-    $mergedContent += $templateContent
-    $mergedContent += $privilegeRightsContent 
-
-    New-Item merged_secpol.cfg -type file
-    Set-Content merged_secpol.cfg $mergedContent -Encoding ASCII
-
-    # Apply new merged config
-    secedit /configure /db c:\windows\security\local.sdb /cfg merged_secpol.cfg /areas SECURITYPOLICY
+    # Apply template config
+    secedit /configure /db c:\windows\security\local.sdb /cfg template3_secpol.cfg /areas SECURITYPOLICY
     gpupdate /force
 
     Write-Host "Security policies configured successfully!" -ForegroundColor Green
